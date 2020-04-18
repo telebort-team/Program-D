43bfc7258429730d0af509e69493e90c767f47e5
Leongys <m1x.drk@gmail.com>
2020-04-18 14:38:35 +0800

    add on project Click Rush

---
+++ Click_Rush/script.js
@@ -0,0 +1,38 @@
+// State important DOMs as variable
+
+
+
+
+// State the important variables
+
+
+
+
+
+// Click event on Press button
+
+
+
+
+
+
+
+
+
+
+//Count function
+
+
+
+
+
+
+
+
+
+
+//Stop game function
+
+
+
+
